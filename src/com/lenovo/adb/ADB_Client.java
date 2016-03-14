4cb35b90ef2b67e979c5f492d811bffecdc21279
梁惠涌 <1026946613@qq.com>
2016-03-14 17:48:02 +0800

    初步通信

--- src/com/lenovo/adb/ADB_Client.java
+++ src/com/lenovo/adb/ADB_Client.java
@@ -1,5 +1,7 @@
 package com.lenovo.adb;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -14,65 +17,84 @@ import javax.xml.stream.events.StartDocument;
 
 public class ADB_Client {
 
-	private static final String HOST = "localhost";  
-    private static final int PORT = 9888;  
-    private static final int Server_Port = 9877;
-    private Socket socket = null;  
-    private static BufferedReader in = null;  
-    private PrintWriter out = null;  
-    
-    public ADB_Client() {
-		// TODO Auto-generated constructor stub
-    	try {
-    		
-    		Runtime.getRuntime().exec("adb" + " forward tcp:"+PORT+" tcp:" + Server_Port);
-			socket = new Socket(HOST, PORT);
-		    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));  
-		    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true); 
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
-      
-    }
-	
+	private static final String HOST = "localhost";
+	private static final int PORT = 9888;
+	private static final int Server_Port = 9877;
+	private static Socket socket = null;
+	private BufferedOutputStream out;
+	private BufferedInputStream in;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ADB_Client adb_Client = new ADB_Client();
-		
-	    if (adb_Client != null) {
-			while (true) {
-				try {
-					String string = in.readLine();
-					if (string != null) {
-						System.out.println(string);
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		adb_Client.init_adb_socket();
+		adb_Client.show_data();
+	}
+
+	private void init_adb_socket() {
+		// TODO 初始化ADB 端口号
+		try {
+			Runtime.getRuntime().exec("adb shell am broadcast -a NotifyServiceStop");
+			Thread.sleep(3000);
+			Runtime.getRuntime().exec("adb forward tcp:12580 tcp:12286");
+			Thread.sleep(3000);
+			Runtime.getRuntime().exec("adb shell am broadcast -a NotifyServiceStart");
+			Thread.sleep(3000);
+		} catch (IOException e3) {
+			e3.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Socket socket = null;
+
+		try {
+			InetAddress serverAddr = null;
+			serverAddr = InetAddress.getByName("127.0.0.1");
+			System.out.println("TCP 1111" + "C: Connecting...");
+			socket = new Socket(serverAddr, 12580);
+			String str = "hi,wufenglong";
+			System.out.println("TCP 221122" + "C:RECEIVE");
+
+			out = new BufferedOutputStream(socket.getOutputStream());
+			in = new BufferedInputStream(socket.getInputStream());
+
+		} catch (UnknownHostException e1) {
+			System.out.println("TCP 331133" + "ERROR:" + e1.toString());
+		} catch (Exception e2) {
+			System.out.println("TCP 441144" + "ERROR:" + e2.toString());
+		}
+
+	}
+
+	private void show_data() {
+		while (true) {
+			String string;
+			if ((string = readFromSocket(in)) != null) {
+				System.out.println(string);
 			}
 		}
 	}
-	
-	//读取socket in数据流
-	private static String readFromSocket(InputStream in){
+
+	/* 从InputStream流中读数据 */
+	public static String readFromSocket(InputStream in) {
 		int MAX_BUFFER_BYTES = 4000;
 		String msg = "";
 		byte[] tempbuffer = new byte[MAX_BUFFER_BYTES];
-		try{
+		try {
 			int numReadedBytes = in.read(tempbuffer, 0, tempbuffer.length);
-			msg = new String(tempbuffer,0, numReadedBytes, "utf-8");
+			msg = new String(tempbuffer, 0, numReadedBytes, "utf-8");
+
 			tempbuffer = null;
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		// Log.v(Service139.TAG, "msg=" + msg);
 		return msg;
 	}
+
 }
